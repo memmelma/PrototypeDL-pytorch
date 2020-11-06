fix softmax call again
uncommented softmax, because CrossEntropyLoss doesn't require it

network.py
@@ -103,5 +103,5 @@ class DenseLayerSoftmax(nn.Module):
 
     def forward(self, x):
         out = self.linear(x)
-        out = self.softmax(out)
+#         out = self.softmax(out)
         return out